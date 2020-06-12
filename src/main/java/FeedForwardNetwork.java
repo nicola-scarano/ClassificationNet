3049bf89423d672308e973558e3a58ba64ec4554
nicola <scaranonicola2503@gmail.com>
2020-06-12 11:19:12 +0200

    my first commit

---
+++ src/main/java/FeedForwardNetwork.java
@@ -0,0 +1,11 @@
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+
+public class FeedForwardNetwork {
+
+    public static void main(String[] args) throws Exception {
+
+        RecordReader rr = new CSVRecordReader();
+    }
+
+}
